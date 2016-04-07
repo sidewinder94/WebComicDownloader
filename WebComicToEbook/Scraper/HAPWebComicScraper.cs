dc5c178cb6f71b7d01ea1f93d3467633a479e78d
Antoine-Ali Zarrouk <antoine-ali.zarrouk@soprasteria.com>
2016-04-07 08:29:41 +0100

    First pass at trying to optimize memory usage

--- WebComicToEbook/Scraper/HAPWebComicScraper.cs
+++ WebComicToEbook/Scraper/HAPWebComicScraper.cs
@@ -5,88 +5,90 @@ using System.Xml.XPath;
 
 using Epub;
 
-using WebComicToEbook.Configuration;
-using WebComicToEbook.EmbeddedResources;
-using WebComicToEbook.Properties;
 using HtmlAgilityPack;
 
 using Utils.Text;
 
+using WebComicToEbook.Configuration;
+using WebComicToEbook.EmbeddedResources;
+using WebComicToEbook.Properties;
 using WebComicToEbook.Utils;
 
-using static Utils.Misc.Misc;
 using EPubDocument = Epub.Document;
 
 namespace WebComicToEbook.Scraper
 {
     public class HAPWebComicScraper : BaseWebComicScraper
     {
-        //Valid JSON configuration for this class
-        //{
-        //    "Parser": "XPath",
-        //    "BaseAddress": "http://beyondtheimpossible.org/comic/1-before-the-beginning-2/",
-        //    "NextButtonSelector": "//@href[@class='comic-nav-base comic-nav-next']",
-        //    "ChapterTitleSelector": "//*[@class='post-title']",
-        //    "ChapterContentSelector": "//*[@class='entry']",
-        //    "Author": "Ffurla",
-        //    "Date": "2016-03-18T13:24:36.2855417+01:00",
-        //    "Title": "Beyond the Impossible",
-        //    "Description": null
-        //}
-
+        // Valid JSON configuration for this class
+        // {
+        // "Parser": "XPath",
+        // "BaseAddress": "http://beyondtheimpossible.org/comic/1-before-the-beginning-2/",
+        // "NextButtonSelector": "//@href[@class='comic-nav-base comic-nav-next']",
+        // "ChapterTitleSelector": "//*[@class='post-title']",
+        // "ChapterContentSelector": "//*[@class='entry']",
+        // "Author": "Ffurla",
+        // "Date": "2016-03-18T13:24:36.2855417+01:00",
+        // "Title": "Beyond the Impossible",
+        // "Description": null
+        // }
         protected override void ScrapeWebPage(WebComicEntry entry, Document ebook, string nextPageUrl = null)
         {
-            //http://htmlagilitypack.codeplex.com/wikipage?title=Examples
-            HtmlDocument hDoc = new HtmlDocument();
-            String content = "";
-            String title = "";
-            using (var wc = new WebClient())
+            // http://htmlagilitypack.codeplex.com/wikipage?title=Examples
+            string content = string.Empty;
+            string title = string.Empty;
+
+            try
             {
-                try
+                using (var wc = new WebClient())
                 {
+                    using (var ms = new MemoryStream(wc.DownloadData(nextPageUrl ?? entry.BaseAddress)))
+                    {
+                        HtmlDocument hDoc = new HtmlDocument();
+                        hDoc.Load(ms, true);
+                        XPathNavigator xNav = hDoc.CreateNavigator();
+                        try
+                        {
+                            title = xNav.SelectSingleNode(entry.ChapterTitleSelector).Value;
+                        }
+                        catch
+                        {
+                            ConsoleDisplay.AddAdditionalMessageDisplay(
+                                entry, 
+                                $"Title not found for page {_pageCounter}, replacing with default value");
+                            title = WebUtility.HtmlEncode($"Chapter - {_pageCounter}");
+                        }
 
-                
-                using (var ms = new MemoryStream(wc.DownloadData(nextPageUrl ?? entry.BaseAddress)))
-                {
-                    hDoc.Load(ms, true);
-                    XPathNavigator xNav = hDoc.CreateNavigator();
-                    try
-                    {
-                        title = xNav.SelectSingleNode(entry.ChapterTitleSelector).Value;
+                        content += $"<h1>{title}</h1>";
+                        XPathNodeIterator xIter = xNav.Select(entry.ChapterContentSelector);
+                        while (xIter.MoveNext())
+                        {
+                            var temp = $"<{xIter.Current.Name}>{xIter.Current.Value}</{xIter.Current.Name}>";
+                            content += temp;
+                        }
+
+                        nextPageUrl = xNav.SelectSingleNode(entry.NextButtonSelector)?.Value;
+                        hDoc = null;
+                        xNav = null;
+                        xIter = null;
                     }
-                    catch
-                    {
-                        ConsoleDisplay.AddAdditionalMessageDisplay(entry, $"Title not found for page {_pageCounter}, replacing with default value");
-                        title = WebUtility.HtmlEncode($"Chapter - {_pageCounter}");
-                    }
-                    content += $"<h1>{title}</h1>";
-                    XPathNodeIterator xIter= xNav.Select(entry.ChapterContentSelector);
-                    while (xIter.MoveNext())
-                    {
-                        var temp = $"<{xIter.Current.Name}>{xIter.Current.Value}</{xIter.Current.Name}>";
-                        content += temp;
-                    }
-                    nextPageUrl = xNav.SelectSingleNode(entry.NextButtonSelector)?.Value;
                 }
-                    }
-                catch(WebException ex) 
+            }
+            catch (WebException ex)
+            {
+                if (((HttpWebResponse)ex.Response).StatusCode == HttpStatusCode.NotFound)
                 {
-                    if (((HttpWebResponse)ex.Response).StatusCode == HttpStatusCode.NotFound)
-                    {
-                        return;
-                    }
-                    else
-                    {
-                        ScrapeWebPage(entry, ebook, nextPageUrl);
-                    }
-
+                    return;
+                }
+                else
+                {
+                    ScrapeWebPage(entry, ebook, nextPageUrl);
                 }
             }
 
             AddPage(ebook, content, title);
 
-
-            if(!nextPageUrl.IsEmpty())
+            if (!nextPageUrl.IsEmpty())
             {
                 ScrapeWebPage(entry, ebook, nextPageUrl);
             }
